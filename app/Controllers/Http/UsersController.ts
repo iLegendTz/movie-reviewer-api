5c0a81412a21003ab1a27c113f26918fafb77353
iLegendTz <miguel.isaac@live.com>
2022-06-30 13:36:46 -0500

    [UPDATE] index muestra todos los users.

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,17 +1,34 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import User from 'App/Models/User'
+
 export default class UsersController {
-  public async index({ }: HttpContextContract) { }
+  public async index({ }: HttpContextContract) {
+    /* List all users */
+    return User.all();
+  }
 
-  public async create({ }: HttpContextContract) { }
+  public async create({ }: HttpContextContract) {
+    /* Display a form to create a user */
+  }
 
-  public async store({ }: HttpContextContract) { }
+  public async store({ }: HttpContextContract) {
+    /* Handle user creation form request */
+  }
 
-  public async show({ }: HttpContextContract) { }
+  public async show({ }: HttpContextContract) {
+    /* Return a single user */
+  }
 
-  public async edit({ }: HttpContextContract) { }
+  public async edit({ }: HttpContextContract) {
+    /* Display a form to edit a user */
+  }
 
-  public async update({ }: HttpContextContract) { }
+  public async update({ }: HttpContextContract) {
+    /* Handle user update form submission */
+  }
 
-  public async destroy({ }: HttpContextContract) { }
+  public async destroy({ }: HttpContextContract) {
+    /* Delete user */
+  }
 }
