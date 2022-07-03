225f7dc78bba9698b51e4cf8a8e8d06d24703d13
iLegendTz <miguel.isaac@live.com>
2022-07-03 18:08:52 -0500

    [UPDATE] Columna token ahora es unique [UPDATE] Agregando columna token_type_id

--- database/migrations/1656788216342_tbl_tokens.ts
+++ database/migrations/1656788216342_tbl_tokens.ts
@@ -6,7 +6,8 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('token', 255).notNullable()
+      table.string('token', 255).notNullable().unique()
+      table.integer('token_type_id').unsigned().references('id').inTable('tbl_token_types').notNullable()
       table.integer('user_id').unsigned().references('id').inTable('tbl_users').notNullable()
 
       /**
